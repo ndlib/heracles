85b2feca82b6d5221d8d7be44422467fb96cc66a
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2012-12-12 08:34:28 -0500

    Consolidating integration spec into base spec

--- spec/workers/heracles/worker/always_ok_spec.rb
+++ spec/workers/heracles/worker/always_ok_spec.rb
@@ -9,4 +9,42 @@ describe Heracles::Worker::AlwaysOk do
     Given(:worker) { worker_with_mocked_job(worker_class) }
     Then { worker.process_with_response.should == :ok }
   end
+
+  context 'integration specs', redis: true, slow: true do
+    after(:each) do
+      ResqueSpec.inline = false
+    end
+    context '#process_with_response' do
+      Given(:job) { FactoryGirl.create(:heracles_job) }
+      Given(:workflow_task) {
+        FactoryGirl.create(:heracles_workflow_task,
+                           name: 'always_ok',
+                           job: job
+                           )
+      }
+      Given(:worker) { Heracles::Worker::AlwaysOk.new(workflow_task.job_id) }
+      Then { worker.process_with_response.should == :ok }
+    end
+
+    context '.perform', redis: true, slow: true do
+      Given(:job) { FactoryGirl.create(:heracles_job) }
+      Given(:workflow_task) {
+        FactoryGirl.create(:heracles_workflow_task,
+                           name: 'always_ok',
+                           job: job
+                           )
+      }
+
+      When {
+        with_resque {
+          Heracles::Worker::AlwaysOk.perform(workflow_task.job_id)
+        }
+      }
+      pending "These tests need to be looked at" do
+        Then { workflow_task.reload.status.should == 'completed' }
+        Then { job.reload.status.should == 'completed' }
+      end
+
+    end
+  end
 end
